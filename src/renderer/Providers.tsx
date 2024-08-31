7dddeaeb616995e30e227093d6cf4eb65058e163
Fernando Asulay <fernando.asulay@gmail.com>
2024-08-31 15:41:37 +0200

    Change a bit background color for light theme

--- src/renderer/Providers.tsx
+++ src/renderer/Providers.tsx
@@ -1,11 +1,22 @@
-import { CssVarsProvider } from '@mui/joy';
+import { CssVarsProvider, extendTheme } from '@mui/joy';
 import { FC, PropsWithChildren } from 'react';
 import { AppStateProvider } from './providers/AppStateProvider';
 import { ConfigStateProvider } from './providers/ConfigStateProvider';
 
 export const Providers: FC<PropsWithChildren> = ({ children }) => {
+  const theme = extendTheme({
+    colorSchemes: {
+      light: {
+        palette: {
+          background: {
+            body: '#eee',
+          },
+        },
+      },
+    },
+  });
   return (
-    <CssVarsProvider>
+    <CssVarsProvider theme={theme}>
       <ConfigStateProvider>
         <AppStateProvider>{children}</AppStateProvider>
       </ConfigStateProvider>
--- src/renderer/components/Header.tsx
+++ src/renderer/components/Header.tsx
@@ -1,5 +1,13 @@
 import { FC, PropsWithChildren } from 'react';
-import { Box, Button, IconButton, Sheet, Tooltip, Typography } from '@mui/joy';
+import {
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  Sheet,
+  Tooltip,
+  Typography,
+} from '@mui/joy';
 import {
   AccountCircleOutlined,
   Logout,
@@ -15,59 +23,66 @@ export const Header: FC<PropsWithChildren<{ onClickMenu: () => void }>> = ({
 }) => {
   const { isAuthenticated, logout, currentUser } = useAppState();
   return (
-    <Sheet
-      component="header"
-      className="header"
-      sx={{
-        position: 'sticky',
-        top: 0,
-        zIndex: 100,
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        px: {
-          xs: 2,
-          md: 6,
-        },
-        py: {
-          xs: 2,
-          sm: 2,
-          md: 3,
-        },
-      }}
-    >
-      {/* <LoginModal /> */}
-      <Box display="flex" gap={3}>
-        {isAuthenticated() && (
-          <IconButton variant="outlined" onClick={onClickMenu}>
-            <Menu />
-          </IconButton>
-        )}
-        <Box display="flex" gap={1} alignItems="center">
-          <Typography fontSize={28} display="flex">
-            <ReceiptLongRounded />
-          </Typography>
-          <Typography fontSize={28}>Sanita ticket</Typography>
+    <>
+      <Sheet
+        component="header"
+        className="header"
+        sx={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 100,
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          px: {
+            xs: 2,
+            md: 6,
+          },
+          py: {
+            xs: 2,
+            sm: 2,
+            md: 3,
+          },
+        }}
+      >
+        {/* <LoginModal /> */}
+        <Box display="flex" gap={3}>
+          {isAuthenticated() && (
+            <IconButton variant="outlined" onClick={onClickMenu}>
+              <Menu />
+            </IconButton>
+          )}
+          <Box display="flex" gap={1} alignItems="center">
+            <Typography fontSize={28} display="flex">
+              <ReceiptLongRounded />
+            </Typography>
+            <Typography fontSize={28}>Sanita ticket</Typography>
+          </Box>
         </Box>
-      </Box>
-      <Box display="flex" gap={1} alignItems="center">
-        {isAuthenticated() && (
-          <>
-            <AccountCircleOutlined />
-            <Typography level="title-md">{currentUser?.email}</Typography>
-            <Tooltip
-              title="Haz click para cerrar la session de administrador"
-              color="danger"
-            >
-              <Button endDecorator={<Logout />} color="danger" onClick={logout}>
-                Cerrar sesión
-              </Button>
-            </Tooltip>
-          </>
-        )}
-        <Typography level="title-sm">v{pkg.version}</Typography>
-        <ColorSchemeToggle />
-      </Box>
-    </Sheet>
+        <Box display="flex" gap={1} alignItems="center">
+          {isAuthenticated() && (
+            <>
+              <AccountCircleOutlined />
+              <Typography level="title-md">{currentUser?.email}</Typography>
+              <Tooltip
+                title="Haz click para cerrar la session de administrador"
+                color="danger"
+              >
+                <Button
+                  endDecorator={<Logout />}
+                  color="danger"
+                  onClick={logout}
+                >
+                  Cerrar sesión
+                </Button>
+              </Tooltip>
+            </>
+          )}
+          <Typography level="title-sm">v{pkg.version}</Typography>
+          <ColorSchemeToggle />
+        </Box>
+      </Sheet>
+      <Divider />
+    </>
   );
 };
